0ece2a39a54c0ec763a3baef8252c25095c2197f
Kiprotich Code <fleeklawi@gmail.com>
2024-08-30 10:11:38 +0300

    fix up - user's dashboard

--- backend/main/views.py
+++ backend/main/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from main.models import Shipment, TrackShipment
+from django.http import Http404
 
 # Create your views here.
 def home(request):
@@ -12,21 +13,32 @@ def user_dashboard(request):
     }
     return render(request, 'main/user_dashboard.html', context)
 
+
 def user_shipments(request):
     shipments = Shipment.objects.filter(shipper=request.user)
     tracking_no = request.GET.get('tracking_no')
     status = None
+    shipment_details = None
+    error_message = None
 
     if tracking_no:
-        shipment = get_object_or_404(TrackShipment, tracking_no=tracking_no)
-        status = shipment.status
+        try:
+            track_shipment = get_object_or_404(TrackShipment, tracking_no=tracking_no)
+            status = track_shipment.status
+            shipment_details = track_shipment.shipment  # Capture the related Shipment details
+        except Http404:
+            error_message = "No shipment matches the provided tracking number."
 
     context = {
         'shipments': shipments,
         'status': status,
+        'shipment': shipment_details,
+        'tracking_no': tracking_no,
+        'error_message': error_message,  # Include error message in context
     }
 
     return render(request, 'main/user_shipments.html', context)
+
 
 def received_shipments(request):
     shipments = Shipment.objects.filter(receiver=request.user)
