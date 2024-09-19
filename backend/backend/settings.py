4c2b0353af1ef466924d0d35eba9000b87705b2e
Kiprotich Code <fleeklawi@gmail.com>
2024-09-19 14:40:48 +0300

    allowed host error

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -12,9 +12,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-rptokruy6ubt5g+(w*e982xo+#%3+@qkmjuh!qln_nb)okm*bf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '*.vercel.app', ]
 
 
 # Application definition
