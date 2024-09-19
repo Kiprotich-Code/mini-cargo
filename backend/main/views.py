80d0a9bb1844fdbb4969a6aaf0717d8d7c1684cf
Kiprotich Code <fleeklawi@gmail.com>
2024-09-19 13:49:08 +0300

    setup for hosting

--- backend/main/views.py
+++ backend/main/views.py
@@ -1,10 +1,32 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from main.models import Shipment, TrackShipment
 from django.http import Http404
 
 # Create your views here.
 def home(request):
-    return render(request, 'index.html')
+    shipments = Shipment.objects.all()
+    tracking_no = request.GET.get('tracking_no')
+    status = None
+    shipment_details = None
+    error_message = None
+
+    if tracking_no:
+        try:
+            track_shipment = get_object_or_404(TrackShipment, tracking_no=tracking_no)
+            status = track_shipment.status
+            shipment_details = track_shipment.shipment  # Capture the related Shipment details
+        except Http404:
+            error_message = "No shipment matches the provided tracking number provided!"
+
+    context = {
+        'shipments': shipments,
+        'status': status,
+        'shipment': shipment_details,
+        'tracking_no': tracking_no,
+        'error_message': error_message,  # Include error message in context
+    }
+
+    return render(request, 'index.html', context)
 
 def user_dashboard(request):
     shipments = Shipment.objects.filter(shipper=request.user)
