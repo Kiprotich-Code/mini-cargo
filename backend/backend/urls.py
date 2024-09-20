68d84878be171412c9472b6b22a2ab8c7ab5d005
Kiprotich Code <fleeklawi@gmail.com>
2024-09-20 13:44:10 +0300

    admin dashboard

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -7,3 +9,7 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('control/', include('control.urls')),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, documents_root=settings.STATIC_ROOT)
