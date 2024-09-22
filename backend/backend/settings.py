d0d352261da92a2d6939022506ce2e563ef0b517
Kiprotich Code <fleeklawi@gmail.com>
2024-09-22 09:33:48 +0300

    uncommitted changes

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-rptokruy6ubt5g+(w*e982xo+#%3+@qkmjuh!qln_nb)okm*bf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = ['127.0.0.1', 'cargo-app-5awf.vercel.app' ]
 
 
@@ -70,23 +70,23 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',  # Use the PostgreSQL backend
-        'NAME': 'railway',  # The name of the database
-        'USER': 'postgres',  # Database user
-        'PASSWORD': 'cjLZsFsCvResSqipSLoXCWfEyrPHhFii',  # Password for the database user
-        'HOST': 'junction.proxy.rlwy.net',  # Database host
-        'PORT': '22050',  # Port number
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',  # Use the PostgreSQL backend
+#         'NAME': 'railway',  # The name of the database
+#         'USER': 'postgres',  # Database user
+#         'PASSWORD': 'cjLZsFsCvResSqipSLoXCWfEyrPHhFii',  # Password for the database user
+#         'HOST': 'junction.proxy.rlwy.net',  # Database host
+#         'PORT': '22050',  # Port number
+#     }
+# }
 
 
 
